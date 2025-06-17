3bf2d47afbb9d1cf6b5b3db1e841dec0476f77f9
Atithi Singh <singhatithi02@gmail.com>
2025-06-17 04:58:09 +0000

    Added React-Router setup, ToastNotification-Container, initialized tailwind for styling

--- vectraq-frontend/src/App.jsx
+++ vectraq-frontend/src/App.jsx
@@ -1,15 +1,32 @@
 import React from "react";
+import CustomToastContainer from "./services/toastContainer/CustomToastContainer";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const notify = () => {
+    toast("👋 Hello, this is a toast notification!");
+  };
+
   return (
     <>
-      <main className="min-h-screen bg-gray-100 text-gray-900 font-sans">
-        <Home />
+      <nav className="bg-white text-black p-4 flex justify-center gap-6 shadow">
+        <Link to="/" className="hover:underline">
+          Home
+        </Link>
+        <Link to="/about" className="hover:underline">
+          About
+        </Link>
+      </nav>
+
+    <main className="min-h-screen font-sans flex items-center justify-center bg-gray-900 text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<Home />} />
+        </Routes>
       </main>
-      <ToastContainer position="top-right" autoClose={3000} />
+
+      <CustomToastContainer  />
     </>
   );
 }
